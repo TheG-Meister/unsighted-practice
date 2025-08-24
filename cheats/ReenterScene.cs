cba48663d6a1778134d62443dfbdfe89fec8daf4
The G-Meister <thegiemeister@gmail.com>
2025-08-24 16:21:05 +0100

    record running, spinner and mecha states on scene change

--- cheats/ReenterScene.cs
+++ cheats/ReenterScene.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
+using static dev.gmeister.unsighted.practice.data.SceneChangeData;
 
 namespace dev.gmeister.unsighted.practice.cheats;
 
@@ -57,6 +58,35 @@ public class ReenterScene
                     ScreenTransition.direction = sceneChangeData.screenTransitionDirection;
                     ScreenTransition.directionPlayerCamerFrom = Helpers.Get8AxisDirection(sceneChangeData.screenTransitionPlayerDirection);
                     ScreenTransition.playerTransitioningScreens = true;
+
+                    if (sceneChangeData.playerSceneChangeData != null)
+                    {
+                        if (sceneChangeData.playerSceneChangeData.Count > 0)
+                        {
+                            PlayerSceneChangeData playerData = sceneChangeData.playerSceneChangeData[0];
+                            ScreenTransition.player1Running = playerData.running;
+                            ScreenTransition.player1StartedRunningWithDoubleTap = playerData.runningWithDoubleTap;
+                            ScreenTransition.player1Spinner = playerData.spinner;
+                            ScreenTransition.player1Mecha = playerData.mecha;
+                        }
+                        if (sceneChangeData.playerSceneChangeData.Count > 1)
+                        {
+                            PlayerSceneChangeData playerData = sceneChangeData.playerSceneChangeData[1];
+                            ScreenTransition.player2Running = playerData.running;
+                            ScreenTransition.player2StartedRunningWithDoubleTap = playerData.runningWithDoubleTap;
+                            ScreenTransition.player2Spinner = playerData.spinner;
+                            ScreenTransition.player2Mecha = playerData.mecha;
+                        }
+
+                        foreach (PlayerSceneChangeData data in sceneChangeData.playerSceneChangeData)
+                        {
+                            if (data.mecha)
+                            {
+                                MechaController.mechaTransitioningScreens = true;
+                                break;
+                            }
+                        }
+                    }
                 }
                 else if (sceneChangeData.transitionType == typeof(SceneChangeLadder)) SceneChangeLadder.currentLadder = sceneChangeData.transitionObject;
                 else if (sceneChangeData.transitionType == typeof(CraterTowerElevator)) CraterTowerElevator.currentElevator = sceneChangeData.transitionObject;
@@ -78,7 +108,15 @@ public class ReenterScene
 
         if (ScreenTransition.playerTransitioningScreens)
         {
-            sceneChangeData = new(scene, typeof(ScreenTransition), ScreenTransition.currentDoorName, ScreenTransition.lastSceneName, ScreenTransition.direction, Helpers.AxisDirectionToVector3(ScreenTransition.directionPlayerCamerFrom));
+            sceneChangeData = new(
+                scene,
+                typeof(ScreenTransition),
+                ScreenTransition.currentDoorName,
+                ScreenTransition.lastSceneName,
+                ScreenTransition.direction,
+                Helpers.AxisDirectionToVector3(ScreenTransition.directionPlayerCamerFrom),
+                new PlayerSceneChangeData(ScreenTransition.player1Running, ScreenTransition.player1StartedRunningWithDoubleTap, ScreenTransition.player1Spinner, ScreenTransition.player1Mecha),
+                new PlayerSceneChangeData(ScreenTransition.player2Running, ScreenTransition.player2StartedRunningWithDoubleTap, ScreenTransition.player2Spinner, ScreenTransition.player2Mecha));
         }
         else if (HoleTeleporter.fallingDownOnHole) sceneChangeData = new(scene, typeof(HoleTeleporter));
         else if (Elevator.ridingElevator) sceneChangeData = new(scene, typeof(Elevator));
--- data/SceneChangeData.cs
+++ data/SceneChangeData.cs
@@ -12,18 +12,39 @@ namespace dev.gmeister.unsighted.practice.data;
 public class SceneChangeData
 {
 
+    [Serializable]
+    public class PlayerSceneChangeData
+    {
+        public readonly bool running;
+        public readonly bool runningWithDoubleTap;
+        public readonly bool spinner;
+        public readonly bool mecha;
+
+        public PlayerSceneChangeData(bool running, bool runningWithDoubleTap, bool spinner, bool mecha)
+        {
+            this.running = running;
+            this.runningWithDoubleTap = runningWithDoubleTap;
+            this.spinner = spinner;
+            this.mecha = mecha;
+        }
+
+        public PlayerSceneChangeData(PlayerSceneChangeData other) : this(other.running, other.runningWithDoubleTap, other.spinner, other.mecha)
+        { }
+    }
+
     public string scene;
     public Type transitionType;
     public string transitionObject;
     public string lastScene;
     public Vector3 screenTransitionDirection = default;
     public Vector3 screenTransitionPlayerDirection = default;
+    public List<PlayerSceneChangeData> playerSceneChangeData = null;
 
     public SceneChangeData()
     {
     }
 
-    public SceneChangeData(string scene, Type transitionType, string transitionObject = null, string lastScene = null, Vector3 screenTransitionDirection = default, Vector3 screenTransitionPlayerDirection = default)
+    public SceneChangeData(string scene, Type transitionType, string transitionObject = null, string lastScene = null, Vector3 screenTransitionDirection = default, Vector3 screenTransitionPlayerDirection = default, params PlayerSceneChangeData[] playerSceneChangeData)
     {
         this.scene = scene;
         this.transitionType = transitionType;
@@ -31,6 +52,7 @@ public class SceneChangeData
         this.lastScene = lastScene;
         this.screenTransitionDirection = screenTransitionDirection;
         this.screenTransitionPlayerDirection = screenTransitionPlayerDirection;
+        this.playerSceneChangeData = new(playerSceneChangeData);
     }
 
     public SceneChangeData(SceneChangeData other)
@@ -41,6 +63,8 @@ public class SceneChangeData
         this.lastScene = other.lastScene;
         this.screenTransitionDirection = other.screenTransitionDirection;
         this.screenTransitionPlayerDirection = other.screenTransitionPlayerDirection;
+        this.playerSceneChangeData = new();
+        if (other.playerSceneChangeData != null) foreach (PlayerSceneChangeData data in other.playerSceneChangeData) this.playerSceneChangeData.Add(new(data));
     }
 
 }
